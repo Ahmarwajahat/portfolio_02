f20bafefe25d22329063607ba29060c3cf6b08bb
Ahmarwajahat <ahmarwajhatawan@gmail.com>
2026-04-01 02:38:28 -0400

    Fix frontend API URLs for production deployment

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -2,4 +2,5 @@ const serverless = require('serverless-http');
 const app = require('../server');
 
 // Create a serverless handler wrapper around our Express app
-module.exports.handler = serverless(app);
+// The basePath prevents 404s when Netlify redirects from /api to /.netlify/functions/api
+module.exports.handler = serverless(app, { basePath: '/.netlify/functions/api' });
